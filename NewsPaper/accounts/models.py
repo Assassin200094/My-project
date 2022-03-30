ba182f2684f13b0ce5462c53667e420ec274d083
Assassin20094 <thepunisher20199@gmail.com>
2022-03-30 19:04:19 +0300

    Новая работа

--- NewsPaper/accounts/models.py
+++ NewsPaper/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Staff(models.Model):
+    full_name = models.CharField(max_length = 255)
+    position = models.CharField(max_length = 255)
+    labor_contract = models.IntegerField()
+
+class Author(models.Model):
+    full_name = models.CharField()
+    name = models.CharField(null=True)
+
+    def some_method(self):
+        self.name = self.full_name.split()[0]
+        self.save()
+
+Author.some_method()
