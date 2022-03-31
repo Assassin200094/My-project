5b929551bc3d970b89ca117be4b0b1ce9a17cf09
Assassin20094 <thepunisher20199@gmail.com>
2022-03-31 16:31:14 +0300

    Новая работа

--- NewsPaper/accounts/models.py
+++ NewsPaper/accounts/models.py
@@ -5,12 +5,3 @@ class Staff(models.Model):
     position = models.CharField(max_length = 255)
     labor_contract = models.IntegerField()
 
-class Author(models.Model):
-    full_name = models.CharField()
-    name = models.CharField(null=True)
-
-    def some_method(self):
-        self.name = self.full_name.split()[0]
-        self.save()
-
-Author.some_method()
